0c17143840343ce66654454971c0f82f06641e09
pathareprashant5 <pathareprashant5@gmail.com>
2025-01-31 14:13:33 +0530

    updated

--- StreamlitAPP.py
+++ StreamlitAPP.py
@@ -3,11 +3,11 @@ import json
 import traceback
 import pandas as pd
 from dotenv import load_dotenv
-from src.mcqgenerator.utils import read_file,get_table_data
+from mcqgenerator.utils import read_file,get_table_data
 import streamlit as st
 from langchain.callbacks import get_openai_callback
-from src.mcqgenerator.MCQGenerator import generate_evaluate_chain
-from src.mcqgenerator.logger import logging
+from mcqgenerator.MCQGenerator import generate_evaluate_chain
+from mcqgenerator.logger import logging
 
 with open('Response.json', 'r') as file:
     RESPONSE_JSON = json.load(file)
--- build/lib/src/mcqgenerator/MCQGenerator.py
+++ build/lib/src/mcqgenerator/MCQGenerator.py
@@ -6,58 +6,58 @@ from dotenv import load_dotenv
 from mcqgenerator.utils import read_file,get_table_data
 from mcqgenerator.logger import logging
 
-# Importing necessary packages from langchain
+#imporing necessary packages packages from langchain
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from langchain.chains import SequentialChain
 
-# Load environment variables form the .env file
+# Load environment variables from the .env file
 load_dotenv()
 
-# Access the enviroment varaibles just like you would with os.environ
-key=os.getenv("OPENAI_API_KEY")
+# Access the environment variables just like you would with os.environ
+key = os.getenv("OPENAI_API_KEY")
 
+print("Value of MY_VARIABLE:", key)
 
-llm = ChatOpenAI(openai_api_key=key,model_name="gpt-3.5-turbo", temperature = 0.7)
+llm = ChatOpenAI(openai_api_key=key,model_name="gpt-3.5-turbo", temperature=0.3)
 
 template="""
 Text:{text}
 You are an expert MCQ maker. Given the above text, it is your job to \
-create a quiz of {number} multiple choice questions for {subject} students in {tone} tone.
-Make sure the questions are not repeated and check all the questions to be confirming the text as well.
-Make sure to format your response like RESPONS_JSON below and use it as a guide.\
+create a quiz  of {number} multiple choice questions for {subject} students in {tone} tone. 
+Make sure the questions are not repeated and check all the questions to be conforming the text as well.
+Make sure to format your response like  RESPONSE_JSON below  and use it as a guide. \
 Ensure to make {number} MCQs
 ### RESPONSE_JSON
 {response_json}
+
 """
 
-qgp=PromptTemplate(      #qgp-quiz generation prompt
-    input_variables=["text ","number","subject","tone","response_json"],
-    template=template
-)
-
-quiz_chain=LLMChain(llm=llm,prompt=qgp,output_key="quiz",verbose=True)
+quiz_generation_prompt = PromptTemplate(
+    input_variables=["text", "number", "grade", "tone", "response_json"],
+    template=template)
 
 
-template2="""
+quiz_chain=LLMChain(llm=llm, prompt=quiz_generation_prompt, output_key="quiz", verbose=True)
+
+template="""
 You are an expert english grammarian and writer. Given a Multiple Choice Quiz for {subject} students.\
-You need to evaluate the complexity of the question and give a complete analysis of the quiz. Only use max 50 words for complexity.
-If the quiz is not per the cognitive and analytical abilities of the students,\
-update the quiz questions which needs to be changed and change the tone such that it perfectly fits the students ability.
+You need to evaluate the complexity of teh question and give a complete analysis of the quiz if the students
+will be able to unserstand the questions and answer them. Only use at max 50 words for complexity analysis. 
+if the quiz is not at par with the cognitive and analytical abilities of the students,\
+update tech quiz questions which needs to be changed  and change the tone such that it perfectly fits the student abilities
 Quiz_MCQs:
 {quiz}
 
-Check from an expert English writer of the above quiz:
+Check from an expert English Writer of the above quiz:
 """
 
-qep=PromptTemplate(      #qep - quiz evaluation template
-    input_variable=["subject","quiz"], template=template2
-)
+quiz_evaluation_prompt=PromptTemplate(input_variables=["subject", "quiz"], template=template)
 
-review_chain=LLMChain(llm=llm,prompt=qep,output_key="review",verbose=True)
+review_chain=LLMChain(llm=llm, prompt=quiz_evaluation_prompt, output_key="review", verbose=True)
 
+
+# This is an Overall Chain where we run the two chains in Sequence
 generate_evaluate_chain=SequentialChain(chains=[quiz_chain, review_chain], input_variables=["text", "number", "subject", "tone", "response_json"],
                                         output_variables=["quiz", "review"], verbose=True,)
-
-
